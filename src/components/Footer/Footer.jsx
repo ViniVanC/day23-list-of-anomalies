75f10e3e3aa513a9ff77f5677c803e033de33bdf
ViniVanC <vincentvan0302@gmail.com>
2023-04-08 18:39:12 +0300

    created component Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "../Container";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <Container>footer</Container>
+    </footer>
+  );
+};
