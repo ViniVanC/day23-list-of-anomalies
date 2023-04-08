01727032f75c944c9b2688491c61fa8d9f6a7dd6
ViniVanC <vincentvan0302@gmail.com>
2023-04-08 13:39:34 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Container } from "./components/Container";
+import { MarkdownText } from "./components/MarkdownText/MarkdownText";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <header>
+        <Container>header</Container>
+      </header>
+      <main>
+        <Container>
+          <MarkdownText fileUrl={"/test.md"} />
+        </Container>
+      </main>
+      <footer>
+        <Container>footer</Container>
+      </footer>
+    </>
+  );
 }
 
 export default App;
