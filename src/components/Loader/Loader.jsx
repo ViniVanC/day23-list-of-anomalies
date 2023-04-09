3a5f8070cd90feb38d7d2e2848044ca1e5e4a349
ViniVanC <vincentvan0302@gmail.com>
2023-04-09 16:43:48 +0300

    created component Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Loader.scss";
+
+export const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="logo">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
