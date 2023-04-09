9f6a1bb4dd361f9be7e0e660668938bff8131ebb
ViniVanC <vincentvan0302@gmail.com>
2023-04-09 20:08:17 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Container } from "./components/Container";
 import { Header } from "./components/Header/Header";
 import { PagesList } from "./components/Pages/PagesList";
 import { Page } from "./components/Pages/Page";
-import { Intro } from "./sctions/Intro/Intro";
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <main>
         <Container>
-          <Intro />
           <Routes>
             <Route exact path="/" element={<PagesList />} />
             <Route exact path="/pages/:id" element={<Page />} />
           </Routes>
         </Container>
       </main>
-    </Router>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { PagesProvider } from "./hooks/usePages";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <PagesProvider>
+        <App />
+      </PagesProvider>
+    </Router>
   </React.StrictMode>
 );
