412a2140eb5d4f23fea168cc65a3f3bc81f08e1a
ViniVanC <vincentvan0302@gmail.com>
2023-04-14 16:06:18 +0300

    added saving theme value to localStorage

--- src/hooks/useTheme.jsx
+++ src/hooks/useTheme.jsx
@@ -1,7 +1,14 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 
 export const useTheme = () => {
-  const [theme, setTheme] = useReducer((t) => !t, true);
+  const [theme, setTheme] = useReducer(
+    (t) => !t,
+    JSON.parse(localStorage.getItem("day23-user-theme")) || false
+  );
+
+  useEffect(() => {
+    localStorage.setItem("day23-user-theme", JSON.stringify(theme));
+  }, [theme]);
 
   if (theme) {
     document.body.classList.add("light");
